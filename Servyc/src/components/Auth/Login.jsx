3bf1556abfe0096d6dc69c4a96d7567d32876274
zakir19 <patelzakir784@gmail.com>
2025-01-04 12:43:46 +0530

    first commit

---
+++ Servyc/src/components/Auth/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex items-center justify-center'>
+        <div className=''
+      
+    </div>
+  )
+}
+
+export default Login
